fb9166f9b68b642a26142952b2d4a7880e335599
Collin Smith <smith.colin@husky.neu.edu>
2018-05-02 21:54:33 -0700

    lsdhjf

--- src/main/java/edu/neu/ccs/cs5004/eschaton/config/Config.java
+++ src/main/java/edu/neu/ccs/cs5004/eschaton/config/Config.java
@@ -1,6 +1,7 @@
 package edu.neu.ccs.cs5004.eschaton.config;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.awt.*;
 import java.util.Arrays;
@@ -28,7 +29,7 @@ public class Config {
   public static final int Y_STEP_ONE = HEXAGON_SIZE * 2;
   public static final int Y_STEP_TWO = HEXAGON_SIZE * 4;
 
-  public Gson gson = new Gson();
+  public Gson gson = new GsonBuilder().create();
 
   private Boolean fogOfWar;
   private Integer numberOfPlayers;
